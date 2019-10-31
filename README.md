ff15d0d64f34b518d099fa709a0f22713be770fc
Matvey <matwey75@gmail.com>
2019-10-31 18:06:46 +0300

    add all tests

---
+++ README.md
@@ -0,0 +1,15 @@
+# Домашнее задание к занятию «2.1. Тестирование веб-интерфейсов» курса Netology.ru "Тестировщик"
+## Тема: "Тестирование веб-интерфейсов, Selenium и Selenide"
+
+## Отчет по выполнению тестов:
+[![Build status](https://ci.appveyor.com/api/projects/status/hl7mjv43bd6gychx?svg=true)](https://ci.appveyor.com/project/Matvey75/aqa-2-1)
+
+## Задача:
+* выполнение автоматических тестов и настрока CI (ci.appveyor.com);
+* обеспечить запуск целевого сервиса;
+* добавить проверку валидации полей.
+
+## Реализация:
+* Склонировать репозиторий `git clone https://https://github.com/Matvey75/aqa_2_1.git`
+* Перейти в директорию с проектом
+* Выполнить команду `./gradlew test` (`gradlew.bat test`, если запускается из windows)
--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,13 +1,16 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CallbackTest {
     @Test
+    @DisplayName(value = "Should request successfully submitted")
     void shouldSubmitRequest() throws InterruptedException {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Василий Иванов");
@@ -15,5 +18,49 @@ class CallbackTest {
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    @DisplayName(value = "Should error if field 'Name' is empty")
+    void shouldErrorIfNameEmpty() {
+        open("http://localhost:9999");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        SelenideElement nameError = $("[data-test-id=name]");
+        nameError.shouldHave(cssClass("input_invalid"));
+        nameError.$(By.className("input__sub")).shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    @DisplayName(value = "Should error if field 'Name' is incorrect")
+    void shouldErrorIfNameIncorrect() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Vasily Ivanov");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        SelenideElement nameError = $("[data-test-id=name]");
+        nameError.shouldHave(cssClass("input_invalid"));
+        nameError.$(By.className("input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    @DisplayName(value = "Should error if field 'Phone' is empty")
+    void shouldErrorIfPhoneEmpty() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий Иванов");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        SelenideElement phoneError = $("[data-test-id=phone]");
+        phoneError.shouldHave(cssClass("input_invalid"));
+        phoneError.$(By.className("input__sub")).shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    @DisplayName(value = "Should error if field 'Agreement' is empty")
+    void shouldErrorIfAgreementEmpty() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий Иванов");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $(By.className("button")).click();
+        $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
 }
